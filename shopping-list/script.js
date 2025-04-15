224c8ede9b1ac4686aa25a924df1977836430932
Laura Boyle <156213687+Pingrrrr@users.noreply.github.com>
2025-04-15 20:28:25 +0100

    update item and reset state / prevent dupes items

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -4,6 +4,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 function displayItems() {
     const itemsFromStorage = getItemsFromStorage();
@@ -22,6 +24,21 @@ function onAddItemSubmit(e) {
         alert('Please add an item');
         return;
     }
+    //check for edit mode
+
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+    } else {
+        if (checkIfItemExists(newItem)) {
+            alert('that item already exists!');
+            return;
+        }
+    }
+
     // create item DOM element
     addItemToDOM(newItem);
     // add item to local storage
@@ -88,7 +105,26 @@ function getItemsFromStorage() {
 function onClickItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
         removeItem(e.target.parentElement.parentElement);
+    } else {
+        setItemToEdit(e.target);
     }
+}
+
+function checkIfItemExists(item) {
+    const itemsFromStorage = getItemsFromStorage();
+    return itemsFromStorage.includes(item);
+}
+
+function setItemToEdit(item) {
+    isEditMode = true;
+    // get all the list items then for each to remove edit mode
+    itemList.querySelectorAll('li')
+        .forEach((i) => i.classList.remove('edit-mode'));
+
+    item.classList.add('edit-mode');
+    formBtn.innerHTML = '<i class = "fa-solid fa-pen"></i> Update Item';
+    itemInput.value = item.textContent;
+    formBtn.style.backgroundColor = '#228b22'
 }
 
 function removeItem(item) {
@@ -105,7 +141,7 @@ function removeItem(item) {
 
 function removeItemFromStorage(item) {
     let itemsFromStorage = getItemsFromStorage();
-    
+
     //filter out item to be removed
     itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
 
@@ -145,6 +181,7 @@ function filterItems(e) {
 }
 
 function checkUI() {
+    itemInput.value = '';
     //run inside the function so that when this function runs, then it takes the new items
     const items = itemList.querySelectorAll('li');
     if (items.length === 0) {
@@ -154,6 +191,10 @@ function checkUI() {
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+    //reset state
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = '#333';
+    isEditMode = false;
 }
 
 //intialise app
